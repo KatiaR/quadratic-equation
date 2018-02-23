61af1eb3af52cc83cceaef082973b2e3a98ae470
Katia Romaniuk <katia-l@mail.ru>
2018-02-23 22:53:18 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
-}
+    let [a,b,c] = equation.match(/(-\s?\d*|\+\s?\d*|^\d+)/g);
+    a = +a;
+    b = +b.replace(' ','');
+    c = +c.replace(' ','');
+    let x1 = (-b + Math.pow((b*b - 4*a*c),0.5))/2/a;
+    let x2 = (-b - Math.pow((b*b - 4*a*c),0.5))/2/a;
+    if (a > 0) {
+        return [Math.round(x2),Math.round(x1)]
+    } else {
+      return [Math.round(x1),Math.round(x2)]
+    }
+};
